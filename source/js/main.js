b9644740b7e302f61dd38d467eddf8cd59600d9c
Chia Yi Lai <cheesbe@gmail.com>
2016-06-01 20:39:46 +0800

    add lang cookie and judge browser lang

--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,41 @@
 $(function(){
+	// js cookie
+	function setCookie(cname, cvalue, exdays) {
+	    var d = new Date();
+	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	    var expires = "expires="+ d.toUTCString();
+	    document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	            return c.substring(name.length,c.length);
+	        }
+	    }
+	    return "";
+	}
+
 	// jquery translate
-	var translator = $('body').translate({lang: "zhtw", t: dict});
+	var lang = window.navigator.userLanguage || window.navigator.language ;
+	var relang = lang.toLowerCase();
+	var getCookieLang = getCookie('cookieLang');
+	if(getCookieLang){
+		var translator = $('body').translate({lang: getCookieLang, t: dict});
+	}else if(relang === 'zh-tw' || relang === 'zh-cn'){
+		var translator = $('body').translate({lang: "zhtw", t: dict});
+	}else{
+		var translator = $('body').translate({lang: "en", t: dict});
+	}
 	$('.btn-translate').on('click', function(){
 		var lang = $(this).attr('id');
 		translator.lang(lang);
+		setCookie('cookieLang', lang, 365);
 	});
 
 	// nav-main
