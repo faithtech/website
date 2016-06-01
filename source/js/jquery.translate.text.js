16c7ba3cf96420327e725436ba3bed01563c16db
Chia Yi Lai <cheesbe@gmail.com>
2016-06-01 16:42:24 +0800

    add translate.js and translate header text

---
+++ source/js/jquery.translate.text.js
@@ -0,0 +1,40 @@
+var dict = {
+	// header
+  	'What is CHECKIN+ ?': {
+    	zhtw: 'CHECKIN+ 是什麼？',
+    	en: 'What is CHECKIN+ ?'
+  	},
+  	'How to work?': {
+    	zhtw: '如何合作？',
+    	en: 'How to work?'
+  	},
+  	'Our Services': {
+    	zhtw: '加值服務',
+    	en: 'Our Services'
+  	},
+  	'About': {
+    	zhtw: '關於我們',
+    	en: 'About'
+  	},
+  	'Languages': {
+    	zhtw: '語言',
+    	en: 'Languages'
+  	},
+  	// slogan
+  	'・The leader of Exhibition technology': {
+    	zhtw: '會展科技領導者',
+    	en: '・The leader of Exhibition technology'
+  	},
+  	'・Fully functional check-in services': {
+    	zhtw: '・全功能簽到服務',
+    	en: '・Fully functional check-in services'
+  	},
+  	'・More smoothly': {
+    	zhtw: '・讓活動更加順暢',
+    	en: '・More smoothly'
+  	},
+  	'・Send Order': {
+    	zhtw: '・寄信預約',
+    	en: '・Send Order'
+  	},
+}
--- source/js/main.js
+++ source/js/main.js
@@ -1,15 +1,24 @@
 $(function(){
+	// jquery translate
+	var translator = $('body').translate({lang: "zhtw", t: dict});
+	$('.btn-translate').on('click', function(){
+		var lang = $(this).attr('id');
+		translator.lang(lang);
+	});
+
+	// nav-main
 	var mask = document.createElement('div');
 	mask.classList.add('black-mask');
 	document.body.appendChild(mask);
-	// nav-main-toggle
 	document.getElementById('btn-menu').addEventListener('click', function(e){
 		mask.classList.add('show');		
   		document.body.classList.add('nav-main-open');
   		e.stopPropagation();
  		document.addEventListener('click', function(e){
-  			document.body.classList.remove('nav-main-open');
-  			mask.classList.remove('show');
+ 			if(!e.target.closest('#dropdownLang')){
+	  			document.body.classList.remove('nav-main-open');
+	  			mask.classList.remove('show');
+	  		}
   		});
 	});
 
