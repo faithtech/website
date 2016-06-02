0c7826432e8c0a80288f6f395088a7a99af655c4
GINGER <chenyijyun@gmail.com>
2016-06-03 01:23:03 +0800

    Landingpage 第一版
    1. Ajax 還沒有 Api
    2. 多語系翻譯為暫定
    3. 下方見證者照片和 Logo 待補上

---
+++ README.md
@@ -0,0 +1,11 @@
+# website-template
+
+Chia Yi Lai's website template：
+
+1.  使用 PHP 簡單架構
+2.  Gulp: 使用 SASS
+3.  Gulp: JS 合併壓縮
+6.  使用資源：
+  - Bootstrap CSS & JavaScript
+  - Font Awesome Icons
+  - jQuery
---
+++ source/js/main.js
@@ -0,0 +1,118 @@
+$(function(){
+	// js cookie
+	function setCookie(cname, cvalue, exdays) {
+	    var d = new Date();
+	    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	    var expires = "expires="+ d.toUTCString();
+	    document.cookie = cname + "=" + cvalue + "; " + expires;
+	}
+	function getCookie(cname) {
+	    var name = cname + "=";
+	    var ca = document.cookie.split(';');
+	    for(var i = 0; i <ca.length; i++) {
+	        var c = ca[i];
+	        while (c.charAt(0)==' ') {
+	            c = c.substring(1);
+	        }
+	        if (c.indexOf(name) == 0) {
+	            return c.substring(name.length,c.length);
+	        }
+	    }
+	    return "";
+	}
+
+	// jquery translate
+	var lang = window.navigator.userLanguage || window.navigator.language ;
+	var relang = lang.toLowerCase();
+	var getCookieLang = getCookie('cookieLang');
+	if(getCookieLang){
+		var translator = $('body').translate({lang: getCookieLang, t: dict});
+	}else if(relang === 'zh-tw' || relang === 'zh-cn'){
+		var translator = $('body').translate({lang: "zhtw", t: dict});
+	}else{
+		var translator = $('body').translate({lang: "en", t: dict});
+	}
+	$('.btn-translate').on('click', function(){
+		var lang = $(this).attr('id');
+		translator.lang(lang);
+		setCookie('cookieLang', lang, 365);
+	});
+
+	// nav-main
+	var mask = document.createElement('div');
+	mask.classList.add('black-mask');
+	document.body.appendChild(mask);
+	document.getElementById('btn-menu').addEventListener('click', function(e){
+		mask.classList.add('show');		
+  		document.body.classList.add('nav-main-open');
+  		e.stopPropagation();
+ 		document.addEventListener('click', function(e){
+ 			if(!e.target.closest('#dropdownLang')){
+	  			document.body.classList.remove('nav-main-open');
+	  			mask.classList.remove('show');
+	  		}
+  		});
+	});
+
+	// btn-scroll
+	$('.btn-scroll').on('click', function(){
+		var target = $(this.hash).offset().top;
+		$('html, body').animate({'scrollTop': target});
+	});
+
+	// form-booking
+	$('#form-booking').on('submit', function(e){
+		e.preventDefault();
+		var $form = $(this),
+			url = $form.attr('action'),
+			name = $('#booking-name').val(),
+			phone = $('#booking-phone').val(),
+			email = $('#booking-email').val(),
+			eventName = $('#booking-event-name').val(),
+			fromYearSelect = document.getElementById('booking-from-year'),
+			fromYear = fromYearSelect.options[fromYearSelect.selectedIndex].text,
+			fromMonthSelect = document.getElementById('booking-from-month'),
+			fromMonth = fromMonthSelect.options[fromMonthSelect.selectedIndex].text,
+			fromDaySelect = document.getElementById('booking-from-day'),
+			fromDay = fromDaySelect.options[fromDaySelect.selectedIndex].text,
+			toYearSelect = document.getElementById('booking-to-year'),
+			toYear = toYearSelect.options[toYearSelect.selectedIndex].text,
+			toMonthSelect = document.getElementById('booking-to-month'),
+			toMonth = toMonthSelect.options[toMonthSelect.selectedIndex].text,
+			toDaySelect = document.getElementById('booking-to-day'),
+			toDay = toDaySelect.options[toDaySelect.selectedIndex].text,
+			eventAdd = $('#booking-event-add').val(),
+			eventMembers = $('#booking-event-members').val(),
+			other = $('#booking-other').val();
+
+		$.ajax({
+           	type: "POST",
+           	url: url,
+	       	data: {
+	       		name: name,
+	       		phone: phone,
+	       		email: email,
+				eventName: eventName,
+				fromYear: fromYear,
+				fromMonth: fromMonth,
+				fromDay: fromDay,
+				toYear: toYear,
+				toMonth: toMonth,
+				toDay: toDay,
+				eventAdd: eventAdd,
+				eventMembers: eventMembers,
+				other: other
+	       	},
+           	dataType: 'json',
+           	success: function(data){
+               	console.log('success');
+               	$('#modal-success').modal('show');
+           	},
+           	error: function(){
+           		console.log('error');
+           		$('#modal-booking').modal('hide');
+           		$('#modal-error').modal('show');
+           	}
+        });    	
+	});
+});
