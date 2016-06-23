0b944ea86451b996b4d1ad0bd5801e853b426abb
Chia Yi Lai <cheesbe@gmail.com>
2016-06-23 14:31:12 +0800

    fix nav-main links

--- in_header.php
+++ in_header.php
@@ -3,9 +3,9 @@
 	<h1 class="logo"><a href="index.php" title="Checkin+"><img src="public/img/logo.png" alt="Checkin+"></a></h1>
 	<nav class="nav-main">
         <ul>
-    		<li><a href="#nav-1" class="btn-scroll trn">What is Checkin+ ?</a></li>
-    		<li><a href="#nav-2" class="btn-scroll trn">How to work?</a></li>
-    		<li><a href="#nav-3" class="btn-scroll trn">Our Services</a></li>
+    		<li><a href="index.php#nav-1" class="btn-scroll trn">What is Checkin+ ?</a></li>
+    		<li><a href="index.php#nav-2" class="btn-scroll trn">How to work?</a></li>
+    		<li><a href="index.php#nav-3" class="btn-scroll trn">Our Services</a></li>
     		<li><a href="pricing.php" class="trn">Price List</a></li>
     		<li><a href="#nav-4" class="btn-scroll trn">About</a></li>
     		<li class="dropdown"><a href="#" id="dropdownLang" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"><span class="trn">Lang</span> <span class="caret"></span></a>
